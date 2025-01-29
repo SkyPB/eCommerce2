c8291aba00155896f99c40dc945ee9dbe904c91d
‘SkyPB’ <skyshanice01@gmail.com>
2025-01-29 12:57:43 -0500

    fixed database connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
 
 const app = express();
 
@@ -11,36 +11,37 @@ app.use(cors());
 dotenv.config();
 
 // Create a connection pool
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+// console.log(process.env.DB_HOST);
 
-// Query the database
-pool.query("SELECT * FROM products", (err, rows) => {
-  if (err) throw err;
-  console.log(rows);
-});
+// // Query the database
+// pool.query("SELECT * FROM products", (err, rows) => {
+//   if (err) throw err;
+//   console.log(rows);
+// });
 
 // MySQL Configs.
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "password",
-//   database: "products_db",
-// });
+const db = mysql.createConnection({
+  host: "localhost" || 
+  user: "root",
+  password: "password",
+  database: "products_db",
+});
 
 // Connect to MySQL
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to MySQL:", err);
-//   } else {
-//     console.log("Connected to MySQL database");
-//   }
-// });
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
 
 // Defining API endpoint to fetch products from DB
 app.get("/products", (req, res) => {
