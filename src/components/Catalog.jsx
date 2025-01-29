4eb7566b613c3f7225f152f827c090fd4c6c1298
‘SkyPB’ <skyshanice01@gmail.com>
2025-01-29 15:53:58 -0500

    fixed

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -10,7 +10,7 @@ export default function Catalog({ addToCart }) {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch("http://localhost:5000/products");
+      const response = await fetch("/api/products");
       const data = await response.json();
       setProducts(data);
       setFilteredProducts(data);
